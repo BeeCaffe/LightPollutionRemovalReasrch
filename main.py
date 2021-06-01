c01daf77b04ee4996c3d13a33da113016af87a98
DESKTOP-GVKS621\SYS <984770193@qq.com>
2021-06-01 14:58:35 +0800

    last commit of mine

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from src.tools.Utils import *
+import numpy as np
+import time
+weight_save_dir = r'F:\tps_weight/'
+row = 2048
+col = 2048
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+mat = np.loadtxt(r'F:\tps_weight/w_0x31.txt')
+st = time.time()
+for i in range(row):
+    for j in range(col):
+        w_file_name = weight_save_dir + 'w_' + str(i) + 'x' + str(j) + '.txt'
+        np.savetxt(w_file_name, mat)
+        process('compute: ', i*col+j, row*col, st, time.time())
+print('Done!')
